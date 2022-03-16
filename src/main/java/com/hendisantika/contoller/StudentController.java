d6374338d74ecd182b247074cea875b29e264cf5
Hendi Santika <hendisantika@gmail.com>
2022-03-17 06:45:30 +0700

    #6 - Add StudentController

---
+++ src/main/java/com/hendisantika/contoller/StudentController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.contoller;
+
+import com.hendisantika.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-testcontainers-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/03/22
+ * Time: 06.45
+ */
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+}
