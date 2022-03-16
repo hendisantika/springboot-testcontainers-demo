bb1e9b8ace323a55e171cd211b0e21bd74a62e70
Hendi Santika <hendisantika@gmail.com>
2022-03-17 06:45:58 +0700

    #7 - Add createStudent in StudentController

--- src/main/java/com/hendisantika/contoller/StudentController.java
+++ src/main/java/com/hendisantika/contoller/StudentController.java
@@ -1,8 +1,13 @@
 package com.hendisantika.contoller;
 
+import com.hendisantika.entity.Student;
 import com.hendisantika.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,4 +26,10 @@ public class StudentController {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
 }
