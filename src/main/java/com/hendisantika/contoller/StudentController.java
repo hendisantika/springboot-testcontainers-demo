c4dbaed20a0e3d896e5cc3180bbfcbf8704e8e98
Hendi Santika <hendisantika@gmail.com>
2022-03-17 06:46:19 +0700

    #8 - Add getAllStudents in StudentController

--- src/main/java/com/hendisantika/contoller/StudentController.java
+++ src/main/java/com/hendisantika/contoller/StudentController.java
@@ -4,11 +4,14 @@ import com.hendisantika.entity.Student;
 import com.hendisantika.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +34,10 @@ public class StudentController {
     @ResponseStatus(HttpStatus.CREATED)
     public Student createStudent(@RequestBody Student student) {
         return studentRepository.save(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
     }
 }
